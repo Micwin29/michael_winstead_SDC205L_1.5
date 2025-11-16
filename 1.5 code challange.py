29e47718fe26737ef11bae1dd30b520957c5e786
micwin2135 <micwin2135@students.ecpi.edu>
2025-11-16 09:52:48 -0500

    Create 1.5 code challange.py
    first commit

---
+++ 1.5 code challange.py
@@ -0,0 +1,8 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+from datetime import datetime
+
+# Get user input for student ID and age
+student_id = input("MicWin2135: ")
+age = int(input("35: "))
